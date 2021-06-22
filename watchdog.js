5b75d19ca10cd3e9f76a2fd1f09b7ef47120b7d1
Spidersbox <spider@spidersbox.com>
2021-06-22 05:52:50 -0700

    added new user message

--- watchdog.js
+++ watchdog.js
@@ -1,6 +1,8 @@
 // Import the discord.js module
 const Discord = require('discord.js');
+const mysql = require('mysql');
 const config=require("./config.json");
+const pool = require('./database');
 const fs = require('fs');
 // Create an instance of Discord that we will use to control the bot
 const bot = new Discord.Client();
@@ -8,7 +10,14 @@ const bot = new Discord.Client();
 // Token for your bot, located in the Discord application console - https://discordapp.com/developers/applications/me/
 const token =config.token;
 const prefix =config.prefix;
-const logpath="/home/yourname/Discord-bot/Watchdog/";
+const logpath="/home/**yourname**/Discord-bot/Watchdog/";
+
+const sqluser=config.sqluser;
+const sqlpass=config.sqlpass;
+const sqldatabase=config.sqldatabase;
+
+
+
 var logname="message.log";
 try
 { //  rotate the log - it rewites to the same file each time you restart
@@ -30,12 +39,120 @@ var done=0;
 
 bot.on('error', console.error);
 
+//***************************************************************************************  on ready
 // Gets called when our bot is successfully logged in and connected
 bot.on('ready', () =>
 {
     console.log('Watchdog on duty.');
 });
 
+
+//***************************************************************************************  on guild member add
+// Gets called when a new user joins our guild
+bot.on("guildMemberAdd", async (member) =>
+{
+  var mess="New member\n";
+
+  var gserver=member.guild.name;
+mess=mess+"server = "+gserver;
+
+
+  const guild = member.guild;
+mess=mess+"\nguild = "+guild;
+
+const born =member.user.createdAt;
+mess=mess+"\ncreated "+born;
+
+//const age= Date.now() - member.user.createdAt < 1000*60*60*24*10;
+const age= Date.now() - born;
+
+  var tDuration = getDuration(age);
+console.log("duration "+tDuration.value + ': ' + tDuration.unit);
+
+  mess=mess+"\nage = "+tDuration.value+" "+tDuration.unit;
+
+
+
+  console.log(mess);
+  logfile.write(mess);
+
+
+
+  const channel = member.guild.channels.cache.find(channel => channel.name === "landing-pad")
+  if (!channel) return;
+
+  mess=`Welcome ${member} we hope you enjoy your stay here!`;
+  mess=mess+"\n discord account created "+born+" about "+tDuration.value+" - "+tDuration.unit+" ago";
+
+  const joinembed = new Discord.MessageEmbed()
+    .setTitle(`A new member just arrived!`)
+//  .setDescription(`Welcome ${member} we hope you enjoy your stay here!`)
+    .setDescription(mess)
+    .setColor("#FF0000")
+  channel.send(joinembed);
+
+//    channel.send(mess);
+
+
+//  newUsers.set(member.id, member.user);
+
+// just to keep it easier to follow
+  var dbname=member.user.username;
+  var dbusernumber=member.user.discriminator;
+  var dbuserid=member.user.id;
+  var dbborn=new Date(born).toISOString().slice(0, 19).replace('T', ' ');
+  var dbage=tDuration.value;
+  var dbguild=guild.id;
+  var dbgserver=guild.name;
+
+  ret=await updateUserBase(dbname,dbusernumber,dbuserid,dbborn,dbage,dbguild,dbgserver);
+
+//console.log("ret from updateUserBase=",ret);
+
+console.log("dbname       ",dbname);
+console.log("dbusernumber ",dbusernumber);
+console.log("dbuserid     ",dbuserid);
+console.log("dbborn       ",dbborn);
+console.log("dbage        ",dbage);
+console.log("dbguild      ",dbguild);
+console.log("dbgserver    ",dbgserver);
+
+
+});
+
+//*****************************************************************  get duration
+function getDuration(milli){
+  let minutes = Math.floor(milli / 60000);
+  let hours = Math.round(minutes / 60);
+  let days = Math.round(hours / 24);
+
+  return (
+    (days && {value: days, unit: 'days'}) ||
+    (hours && {value: hours, unit: 'hours'}) ||
+    {value: minutes, unit: 'minutes'}
+  )
+};
+
+//*****************************************************************  update userbase
+async function updateUserBase(dbname,dbusernumber,dbuserid,dbborn,dbage,dbguild,dbgserver)
+{
+  try
+  {
+    let result = await pool.query('insert into users(name,usernumber,userid,born,age,guild,gserver) VALUES (\''+dbname+'\',\''+dbusernumber+'\',\''+dbuserid+'\',\''+dbborn+'\',\''+dbage+'\',\''+dbguild+'\',\''+dbgserver+'\')');
+//console.log("insert result=",result);
+    return(result);
+  }
+  catch(err)
+  {
+    console.log('updateUserBase Error occurred', err);
+  }
+
+}
+
+
+//console.log(tDuration.value + ': ' + tDuration.unit);
+
+//***************************************************************************************  on message
 // Event to listen to messages sent to the server where the bot is located
 bot.on('message', message =>
 {
@@ -43,7 +160,8 @@ bot.on('message', message =>
   if (message.author.bot) return;
   var type=message.channel.type;
 
-console.log("message type=",type);
+//console.log("message type=",type);
+
 //  user=message.author.username;
 //  authorid=message.author.discriminator;
   if(message.channel.type=="dm")
@@ -99,15 +217,43 @@ console.log("The file was saved!");
   spamtext="spammers will be removed.";
   mess=message.content.trim().toLowerCase();
   
-  if((mess.includes('uni-airdrop'))&&(mess.includes('uniswap')))
-  {
-    spam=1;
-  }
-  if(mess.includes('uniswap airdrop'))
+
+//nice uniswap
+//uni-claim.io
+  if((mess.includes('airdrop'))&&(mess.includes('uniswap')))
   {
     spam=1;
   }
 
+  if((mess.includes('uni-airdrop'))&&(mess.includes('uniswap')))
+  {
+    spam=1;
+  }
+
+  if(mess.includes('second uniѕwap'))
+  {
+    spam=1;
+  }
+
+  if(mess.includes('axieinfinity.com'))
+  {
+    spam=1;
+  }
+
+  if(mess.includes('uniswap.org'))
+  {
+    spam=1;
+  }
+
+  if(mess.includes('uni-claim'))
+  {
+    spam=1;
+  }
+
+
+
+
+//*****************************************************************  ens spam filter
   var authorname=message.author.username;
   var authorid=message.author.discriminator;
   var uname=message.author.tag;
@@ -116,18 +262,18 @@ console.log("The file was saved!");
   var nick="not set";
   var uid=message.author.id;
 
-console.log("authorname ",authorname);
-console.log("authorid   ",authorid);
-console.log("uid   ",uid);
-console.log("uname      ",uname);
-console.log("guild      ",guild);
+//console.log("authorname ",authorname);
+//console.log("authorid   ",authorid);
+//console.log("uid   ",uid);
+//console.log("uname      ",uname);
+//console.log("guild      ",guild);
 
 
   if(message.member)
     if(message.member.nickname)
       nick=message.member.nickname;
 
-console.log("nickname   ",nick);
+//console.log("nickname   ",nick);
 
   var text=" this was posted in channel "+message.channel+", and made by "+author;
   text=text+"\ntag ="+uname;
@@ -179,17 +325,34 @@ console.log("nickname   ",nick);
     }
   }
 
+
+//********************************************************************************************************  if spam
   if(spam==1)
   {
-    message.channel.send(spamtext+"\n user id "+uid);
-//      message.channel.send(text);
-//      message.channel.send(text2);
-    message.member.ban({reason: 'Spamming!',days: 7});
-//    message.delete();
-    mess=server+"** banned "+message.author.username+"  "+getDateTime()+"\n";
-    logfile.write(mess);
-    console.log(mess);
-    message.channel.send(mess);
+// temp disable ban
+//    message.member.ban({reason: 'Spamming!',days: 7});
+
+
+    var roles="";
+    message.member.roles.cache.forEach(role =>
+    {
+      if(role.name != "\@everyone")
+        roles=roles+role.name+"\n";
+    });
+
+    if(roles.length == 0)
+    {
+      mess=server+"** banned "+message.author.username+"  "+getDateTime()+"\n";
+// temp message
+      mess=server+"** this user should be banned "+message.author.username+"  "+getDateTime()+"\n";
+//      mess=mess+"roles "+roles;
+//      mess=mess+" length "+roles.length;
+
+
+      logfile.write(mess);
+      console.log(mess);
+      message.channel.send(mess);
+    }
   }
 
 });
